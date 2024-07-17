e8336212609e034dcb3e03105ce91e61f0fbe9a0
ProMountain <developer@promountain.at>
2024-07-17 16:35:31 +0200

    Extra error commit

--- ProNatur-Biomarkt GmbH/LoadingScreen.Designer.cs
+++ ProNatur-Biomarkt GmbH/LoadingScreen.Designer.cs
@@ -29,13 +29,10 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoadingScreen));
             this.loadingProgressbar = new System.Windows.Forms.ProgressBar();
             this.label1 = new System.Windows.Forms.Label();
             this.lblLoadingProgress = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.loadingbarTimer = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // loadingProgressbar
@@ -68,15 +65,6 @@
             this.lblLoadingProgress.TabIndex = 2;
             this.lblLoadingProgress.Text = "0%";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(687, 253);
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
-            // 
             // loadingbarTimer
             // 
             this.loadingbarTimer.Interval = 20;
@@ -89,7 +77,6 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
             this.ClientSize = new System.Drawing.Size(684, 318);
             this.ControlBox = false;
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblLoadingProgress);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.loadingProgressbar);
@@ -99,7 +86,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ProNatur-Biomarkt GmbH";
             this.Load += new System.EventHandler(this.LoadingScreen_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,7 +96,6 @@
         private System.Windows.Forms.ProgressBar loadingProgressbar;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblLoadingProgress;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer loadingbarTimer;
     }
 }
